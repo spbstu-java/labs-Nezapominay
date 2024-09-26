11e7aef9ece36495b6b77da80442d890091ff41f
Nezapominay <nepomnyashiy2002@gmail.com>
2024-09-26 18:03:35 +0300

    Added a strategy change with console.

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -1,16 +1,47 @@
 package lab1;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Hero hero1 = new Hero("Иван", new MoveByFly(), PlaceType.Camp, PlaceType.Tower);
-        hero1.move();
-        hero1.setMovement(new MoveByWalk());
-        hero1.move();
-        hero1.setMovement(new MoveByHorse());
-        hero1.move();
-        Hero hero2 = new Hero("Петя", new MoveBySwim(), PlaceType.River, PlaceType.Town);
-        hero2.move();
+
+        System.out.println("Выберите как герой будет перемещаться " +
+                "(ввод 0 прерывает программу): \n" +
+                "1. Пешком \n" +
+                "2. На лошади \n" +
+                "3. Лететь \n" +
+                "4. Плыть \n");
+        Scanner scan = new Scanner(System.in);
+        int changeStrategy = scan.nextInt();
+        while (changeStrategy != 0)
+        {
+            switch (changeStrategy)
+            {
+                case 1:
+                    hero1.setMovement(new MoveByWalk());
+                    hero1.move();
+                    break;
+                case 2:
+                    hero1.setMovement(new MoveByHorse());
+                    hero1.move();
+                    break;
+                case 3:
+                    hero1.setMovement(new MoveByFly());
+                    hero1.move();
+                    break;
+                case 4:
+                    hero1.setMovement(new MoveBySwim());
+                    hero1.move();
+                    break;
+                default:
+                    System.out.println("Введите корректное число " +
+                            "(1-4 или 0 чтобы завершить): ");
+            }
+            changeStrategy = scan.nextInt();
+
+        }
     }
 }

6ee0c4c794af04e633984c82770470240e36eafa
Nezapominay <nepomnyashiy2002@gmail.com>
2024-11-11 16:15:23 +0300

    Task comleted.

--- src/lab4/Methods.java
+++ src/lab4/Methods.java
@@ -1,2 +1,44 @@
-package lab4;public class Methods {
+package lab4;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Methods {
+    public static Map<Character, String> streamTransformToMap(List<String> list) {
+        return list.stream()
+                .collect(Collectors.toMap(str -> str.charAt(0), str -> str.substring(1)));
+    }
+
+    public static int streamEvenNumbers(int[] numbers) {
+        return Arrays.stream(numbers)
+                .filter(x -> x % 2 == 0)
+                .reduce(0, Integer::sum);
+    }
+
+    public static <T> T streamLastElement(Collection<T> collection) {
+        return collection.stream()
+                .reduce((x, y) -> y)
+                .orElseThrow(() -> new NoSuchElementException("Ни одного элемента не найдено"));
+    }
+
+    public static void streamToUpper() {
+        Stream.of("hello", "world", "Cat", "HOUSE")
+                .map(str -> str.toUpperCase())
+                .map(str -> str = "_new_" + str)
+                .forEach(System.out::println);
+    }
+
+    public static double streamAverage(List<Integer> numbers) {
+        return numbers.stream().mapToDouble(Integer::doubleValue)
+                .reduce(0, (x, y) -> x + y) / numbers.stream().count();
+
+    }
+
+    public static void streamUniqueSquare() {
+        Stream.of(1, 2, 3, 2, 4, 2, 5, 2)
+                .distinct()
+                .map(x -> x*x)
+                .forEach(System.out::println);
+    }
 }
